bcf7e11ec09f5c7415aab19603293342d7c19b2e
Kusuma16 <kusumanetcom@gmail.com>
2023-09-20 00:48:29 +0800

    Add checkout service

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const product = require('./services/product');
+const checkout = require('./services/checkout');
 const bodyParser = require('body-parser');
 const app = express();
 
 app.use(bodyParser.json());
-app.use('/product', product);
+app.use('/checkout', checkout);
 
 app.get('/', (req, res) => {
     res.send('Homepage');
