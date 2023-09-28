87be48d07195e34c9a9538161649d06c167e39d3
Kusuma16 <kusumanetcom@gmail.com>
2023-09-28 20:36:43 +0800

    Adding timeout request

--- tester/main.py
+++ tester/main.py
@@ -1,15 +1,18 @@
 from multiprocessing import Pool
-from requests import get,post
+from requests import get
 
 URL = 'http://nodejs-alb-621790754.ap-southeast-1.elb.amazonaws.com/product'
 
 def send_request(val):
-    while True:
-        res = get(f'{URL}')
-        data = res.json()
-        print('Sending request')
-        print(data)
+   try:
+        while True:
+            res = get(f'{URL}', timeout=30)
+            data = res.json()
+            print('Sending request')
+            print(data)
+   except Exception as e:
+        print(e)
 
 if __name__ == '__main__':
     with Pool(50) as p:
-        p.map(send_request, range(50))
+        p.map(send_request, URL)
